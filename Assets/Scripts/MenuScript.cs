99e8a82205f8edd2e0fe99e7eabe77fac5d7e018
floDV1996 <floriandv1996@gmail.com>
2017-04-05 12:27:12 +0200

    First commit

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+
+    private string lastLevel;
+
+    public void Start()
+    {
+        lastLevel = PlayerPrefs.GetString("lastLevel");
+        if (lastLevel == "" || lastLevel == null)
+        {
+            lastLevel = "Level1"; //If there is no last level finished, the user plays the first level
+        }
+    }
+
+    public void LoadLevel(string level) //Load last finished level
+    {
+        SceneManager.LoadScene(lastLevel);
+    }
+
+    public void Settings(string settings) //Open the settings menu (Luminiosity and sound)
+    {
+        SceneManager.LoadScene(settings);
+    }
+
+    public void LevelsSelection(string levelsSelection) //Launch the levels selection menu
+    {
+        SceneManager.LoadScene(levelsSelection);
+    }
+
+    public void quit() //Quit the game
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    public float speed;
+    private Rigidbody player;
+
+    //Variables initialization
+    void Start()
+    {
+        player = GetComponent<Rigidbody>();
+    }
+
+    // Updates every second
+    void FixedUpdate()
+    {
+
+        if (SystemInfo.deviceType == DeviceType.Desktop)
+        { //Code for desktop use
+
+            float HorizontalMove = Input.GetAxis("Horizontal"); //Collecting values of the input
+            float VerticalMove = Input.GetAxis("Vertical");
+
+            Vector3 move = new Vector3(HorizontalMove, 0.0f, VerticalMove); //Initialization of a vector for the player's move
+            player.AddForce(move * speed); //Player is moving ! Higher the speed is, faster my player will be
+
+        }
+        else
+        {  //Code for mobile use
+
+            float HorizontalMove = Input.acceleration.x; //Collecting values of the mobile accelerometer
+            float VerticalMove = Input.acceleration.y;
+
+            Vector3 move = new Vector3(HorizontalMove, 0.0f, VerticalMove); //Same code for the desktop version and the mobile version
+            player.AddForce(move * speed);
+        }
+    }
+}
