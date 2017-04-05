c0cfbe154484402fdf14acbc790c4efe1c8a2e90
floDV1996 <floriandv1996@gmail.com>
2017-04-05 22:59:02 +0200

    Add transition to Level3

--- Assets/Scripts/CollisionControl2.cs
+++ Assets/Scripts/CollisionControl2.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionControl2 : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.name == "End") //If the player touch the "end" object, he can play the next level 
+        {
+            PlayerPrefs.SetString("lastLevel", "Level3"); //We save that the player finished the secondLevel
+            gameObject.SetActive(false);
+            Invoke("NextLevel", 3); //Launching next level after 3 seconds
+        }
+    }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene("Level3");
+    }
 }
